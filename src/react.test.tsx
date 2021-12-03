58b01cf22e30ba6bc4ab6d9928502aa28143794a
Gabe Scholz <github@gabe.pizza>
2021-12-02 20:22:04 -0800

    Test to ensure that elements that use state do not re-render unless their state does

--- src/react.test.tsx
+++ src/react.test.tsx
@@ -12,25 +12,98 @@ type State = {
     b: {
       c: string;
     };
+    d: {
+      e: number;
+    };
   };
 };
 
+const initialState = { a: { b: { c: "cool" }, d: { e: 0 } } };
+
 const { StatefulLensProvider } = create<State>();
 
-test("renders", () => {
-  const App = (props: { state: ProxyLens<State> }) => {
-    const [c] = props.state.a.b.c.useState();
+const App = (props: { state: ProxyLens<State> }) => {
+  const [c, setC] = props.state.a.b.c.useState();
 
-    return <div data-testid="element">{c}</div>;
-  };
+  const onClick = () => setC(c + "!");
 
-  render(
-    <StatefulLensProvider initialValue={{ a: { b: { c: "cool" } } }}>
-      {(lens) => <App state={lens} />}
-    </StatefulLensProvider>
+  return (
+    <div data-testid="element" onClick={onClick}>
+      {c}
+    </div>
   );
+};
+
+test("renders", () => {
+  render(<StatefulLensProvider initialValue={initialState}>{(lens) => <App state={lens} />}</StatefulLensProvider>);
 
   const el = screen.getByTestId("element");
 
   expect(el.innerHTML).toEqual("cool");
+});
+
+test("updates", () => {
+  render(<StatefulLensProvider initialValue={initialState}>{(lens) => <App state={lens} />}</StatefulLensProvider>);
+
+  const el = screen.getByTestId("element");
+
+  expect(el.innerHTML).toEqual("cool");
+
+  el.click();
+  el.click();
+  el.click();
+  el.click();
+
+  expect(el.innerHTML).toEqual("cool!!!!");
+});
+
+test("does not re-render adjacent that do not listen to same state elements", () => {
+  const eRenderCount = jest.fn();
+  const bRenderCount = jest.fn();
+
+  const E = React.memo((props: { state: ProxyLens<State> }) => {
+    const [e] = props.state.a.d.e.useState();
+
+    eRenderCount();
+
+    return <div />;
+  });
+
+  const B = React.memo((props: { state: ProxyLens<State> }) => {
+    const [b] = props.state.a.b.useState();
+
+    bRenderCount();
+
+    return <div data-testid="b" data-b={JSON.stringify(b)} />;
+  });
+
+  render(
+    <StatefulLensProvider initialValue={initialState}>
+      {(lens) => {
+        return (
+          <>
+            <App state={lens} />
+            <E state={lens} />
+            <B state={lens} />
+          </>
+        );
+      }}
+    </StatefulLensProvider>
+  );
+
+  const el = screen.getByTestId("element");
+  const b = screen.getByTestId("b");
+
+  expect(eRenderCount).toHaveBeenCalledTimes(1);
+  expect(bRenderCount).toHaveBeenCalledTimes(1);
+  expect(JSON.parse(b.dataset.b ?? "")).toEqual({ c: "cool" });
+
+  el.click();
+  el.click();
+  el.click();
+  el.click();
+
+  expect(eRenderCount).toHaveBeenCalledTimes(1);
+  expect(bRenderCount).toHaveBeenCalledTimes(5);
+  expect(JSON.parse(b.dataset.b ?? "")).toEqual({ c: "cool!!!!" });
 });
