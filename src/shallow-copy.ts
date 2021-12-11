746a869762592542929500b27d73e2d9d1bd01a6
Gabe Scholz <github@gabe.pizza>
2021-12-10 22:07:44 -0800

    Naming

--- src/shallow-copy.ts
+++ src/shallow-copy.ts
@@ -2,19 +2,19 @@ import { isObject } from "./is-object";
 
 const deepCopy = <T>(obj: T): T => JSON.parse(JSON.stringify(obj));
 
-export const shallowCopy = <T>(prev: T): T => {
+export const shallowCopy = <T>(obj: T): T => {
   /**
    * Need to do this check to ensure that referential
-   * equality will only break a single key in `S`
+   * equality will only break a single key in the object
    */
-  if (isObject(prev)) {
-    return { ...prev };
-  } else if (Array.isArray(prev)) {
-    return [...prev] as any as T;
+  if (isObject(obj)) {
+    return { ...obj };
+  } else if (Array.isArray(obj)) {
+    return [...obj] as any as T;
   } else {
     /**
      * If it's something else, make a deep copy just in case.
      */
-    return deepCopy(prev);
+    return deepCopy(obj);
   }
 };
