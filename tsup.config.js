95cc134a2bf5797d946c130bab9f6413f0d29082
Gabe Scholz <github@gabe.pizza>
2021-12-20 23:27:22 -0800

    Fixup config

--- tsup.config.js
+++ tsup.config.js
@@ -1,20 +1,11 @@
 import { defineConfig } from "tsup";
 
-export default defineConfig([
-  {
-    entry: ["src/index.ts"],
-    format: ["cjs"],
-    external: ["react"],
-    sourcemap: true,
-    clean: true,
-    dts: true,
-    keepNames: true,
-  },
-  {
-    entry: ["src/index.ts"],
-    format: ["esm"],
-    external: ["react"],
-    sourcemap: true,
-    keepNames: true,
-  },
-]);
+export default defineConfig({
+  entry: ["src/index.ts"],
+  format: ["cjs", "esm"],
+  external: ["react"],
+  sourcemap: true,
+  clean: true,
+  dts: true,
+  keepNames: true,
+});
