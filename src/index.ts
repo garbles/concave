f6eb1da55ab27e53cf135d6aa2763502ce6a520e
Gabe Scholz <github@gabe.pizza>
2022-01-08 21:23:26 -0800

    Export connection

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import type { ProxyLens } from "./proxy-lens";
-
+export { connection } from "./connection";
+export type { Connection } from "./connection";
 export { createLens } from "./create-lens";
 export { useCreateLens } from "./react";
 export type { Store } from "./store";
